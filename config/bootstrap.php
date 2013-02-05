bb65bad616e10b2512d45de04be977da0ddf183c
Josey Morton <me@joseymorton.com>
2013-02-05 16:45:48 -0600

    working on Dropbox storage adapter and library autoloader

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -5,10 +5,9 @@ use lithium\core\Libraries;
 
 defined('LI3_DROPBOX_PATH') OR define('LI3_DROPBOX_PATH', Libraries::get('li3_dropbox', 'path'));
 
-$info = array(
-	'key' => 'j0c9s7ht5blqcos',
-	'secret' => 'k9hs8b7q1cqzpd7'
-);
+$info = array('key' => 'j0c9s7ht5blqcos', 'secret' => 'k9hs8b7q1cqzpd7');
+
+$token = "eSjZZVpb6eniQi7cb+U3Fy0ycdrfvhJrJRl7mGRzjyWoMIdMohz7i70NhMqK5ZFexs50hyunUofxTzScfHZZ7brx4kSxsJugRKreJWLOATxP7ZGLdmM2Tav9WpONVDmcjc3TU6gNaWVLyc3s6y7UZPRvtnZzqM+HA7zVjEycgy0yVeK8JxuRPOfSQyzRqqCWVmFPwaqodGX8MFYbRq96KA==";
 
 // Call my own autoloader
 Libraries::add('dropbox', array(
@@ -17,17 +16,17 @@ Libraries::add('dropbox', array(
 ));
 
 // Check whether to use HTTPS and set the callback URL
-$protocol = (!empty($_SERVER['HTTPS'])) ? 'https' : 'http';
-$callback = $protocol . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$callback = (!empty($_SERVER['HTTPS'])) ? 'https' : 'http' . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
-$encrypter = new \Dropbox\OAuth\Storage\Encrypter('f783cf299faa2f66f59dce646e4646b3'); // napavalleybloonz
+$encrypter 	= new \Dropbox\OAuth\Storage\Encrypter('f783cf299faa2f66f59dce646e4646b3'); // napavalleybloonz
 
 // Create the storage object, passing it the Encrypter object
-$storage = new \Dropbox\OAuth\Storage\Session($encrypter);
+$storage 	= new \Dropbox\OAuth\Storage\Manual($encrypter, $token);
+$OAuth 		= new \Dropbox\OAuth\Consumer\Curl($info['key'], $info['secret'], $storage, $callback);
+$dropbox 	= new \Dropbox\API($OAuth);
 
-$OAuth = new \Dropbox\OAuth\Consumer\Curl($info['key'], $info['secret'], $storage, $callback);
-$dropbox = new \Dropbox\API($OAuth);
-
-// print_r($dropbox->accountInfo());exit;
+print_r($storage->token());
+print_r($dropbox->accountInfo());
+exit;
 
 ?>
